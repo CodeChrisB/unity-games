b9e349f738452ec7fb9d398ab94481d26247b8e8
Christopher Buchberger <c.buchberger01@gmail.com>
2021-02-24 22:40:24 +0100

    Archer

--- AgarIO/AgarIO/Assets/Code/Movement.cs
+++ AgarIO/AgarIO/Assets/Code/Movement.cs
@@ -1,26 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
- 
-
- 
-
-    // Start is called before the first frame update
-    void Start()
+    public int area = 50;
+    public float speed
     {
-      
+        get { return (float)(moveSpeed / Math.Log(Eat.size/2+2)); }
+        set { speed = value; }
     }
+
+
 
     // Update is called once per frame
+    private bool isPositiveMovement;
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        movement += new Vector3(0f,Input.GetAxis("Vertical"), 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
-    }
+
+        
+        var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+        targetPos.z = transform.position.z;
+        isPositiveMovement = targetPos.x > 0;
+        targetPos.x = Math.Abs(targetPos.x) > 50 ? targetPos.x * (isPositiveMovement ? 1 : -1) : targetPos.x;
+        targetPos.x = Math.Abs(targetPos.y) > 50 ? targetPos.y * (isPositiveMovement ? 1 : -1) : targetPos.y;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+     }
 
 }
